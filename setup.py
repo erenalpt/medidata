382a1c7df65846406469a4064777791f4ed2b4e5
erenalpt <alppp@mail.com>
2023-06-29 23:28:43 +0300

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='medidata',
-    version='0.5.0',
+    version='0.6.0',
     description='A Python library for working with medical image datasets.',
     author='Alperen T.',
     author_email='alperent@mail.com',
